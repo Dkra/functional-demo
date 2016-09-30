2c640a59830dee898f800d96d99aa8743a294e8e
roger.jiang <dkra4741@gmail.com>
2016-10-01 03:04:53 +0800

    update config, add views, routes

--- routes/github.js
+++ routes/github.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('github', {
+    title: 'Connect to Github API'
+  })
 });
 
 module.exports = router;
